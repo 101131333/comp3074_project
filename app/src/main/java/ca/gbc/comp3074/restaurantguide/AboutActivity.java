82d776dba102674a14596078b82755ec915aa129
ljingGBC <lei.jing@georgebrown.ca>
2020-11-21 20:13:35 -0500

    Add Back Arrow in AboutActivity

--- app/src/main/java/ca/gbc/comp3074/restaurantguide/AboutActivity.java
+++ app/src/main/java/ca/gbc/comp3074/restaurantguide/AboutActivity.java
@@ -1,8 +1,12 @@
 package ca.gbc.comp3074.restaurantguide;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import java.util.Objects;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        // set action bar
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    // display back arrow on action bar to navigate back to activity_main
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        onBackPressed();
+        return true;
     }
 }
